1981ebb806d83fe324f67abc72f092503a62268d
Rukeshkumar <rukeshofficial01@gamil.com>
2025-05-03 18:25:40 +0530

    Add build for the project

---
+++ build/static/js/952.0ead69b7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkproject_root=self.webpackChunkproject_root||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.0ead69b7.chunk.js.map
