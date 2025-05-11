040c920aca47f7a5f2f7b492c9caeb2b4a6385ba
Nithin <nrofficialproducts@gmail.com>
2025-05-11 19:10:47 +0530

    user alignment changes

--- src/components/userManagement/UserManagement.jsx
+++ src/components/userManagement/UserManagement.jsx
@@ -15,50 +15,51 @@ const User = ({
 }) => {
   return (
     <div className="shared-layout-container">
-      <div className="item-table-section">
-        <h3 className="title">User Management</h3>
-        <Spin spinning={loader} tip={"Loading..."}>
-          <div className="table-controls">
-            <Input
-              placeholder="Search user"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              style={{ width: "100%" }}
+      <div className="shared-table-section">
+        <div className="item-table-section">
+          <h3 className="title">User Management</h3>
+          <Spin spinning={loader} tip={"Loading..."}>
+            <div className="table-controls">
+              <Input
+                placeholder="Search user"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                style={{ width: "100%" }}
+              />
+            </div>
+
+            <Table
+              dataSource={userList}
+              columns={columns}
+              rowKey="user_id"
+              rowClassName={(record) =>
+                record?.is_active === false ? "inactive-row" : ""
+              }
+              pagination={{ pageSize: 10 }}
             />
-          </div>
+          </Spin>
 
-          <Table
-            dataSource={userList}
-            columns={columns}
-            rowKey="user_id"
-            rowClassName={(record) =>
-              record?.is_active === false ? "inactive-row" : ""
-            }
-            pagination={{ pageSize: 10 }}
-          />
-        </Spin>
-
-        <Modal
-          title="Are you sure you want to delete this item?"
-          open={showPopConfirm}
-          onOk={() => handleDelete(userId)}
-          onCancel={() => {
-            setShowPopConfirm(false);
-            setLoader(false);
-          }}
-          okText="Yes"
-          cancelText="No"
-          okButtonProps={{
-            style: {
-              backgroundColor: "#d6085e",
-              color: "white",
-            },
-          }}
-        >
-          <p>This action cannot be undone.</p>
-        </Modal>
-      </div>
-    </div>
+          <Modal
+            title="Are you sure you want to delete this item?"
+            open={showPopConfirm}
+            onOk={() => handleDelete(userId)}
+            onCancel={() => {
+              setShowPopConfirm(false);
+              setLoader(false);
+            }}
+            okText="Yes"
+            cancelText="No"
+            okButtonProps={{
+              style: {
+                backgroundColor: "#d6085e",
+                color: "white",
+              },
+            }}
+          >
+            <p>This action cannot be undone.</p>
+          </Modal>
+        </div>
+      </div> </div>
   );
 };
 
